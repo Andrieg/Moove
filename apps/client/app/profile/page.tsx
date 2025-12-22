f3851afeac27d5f687c2b5d5a5c6c6e665d92713
Andrieg <gereandrie@yahoo.com>
2025-12-22 00:08:22 +0000

    auto-commit for fc8a938d-0206-43f9-9369-8b03c1434d6a

--- apps/client/app/profile/page.tsx
+++ apps/client/app/profile/page.tsx
@@ -55,7 +55,7 @@ export default function ProfilePage() {
 
   const handleLogout = () => {
     clearToken();
-    router.push("/onboarding");
+    router.push("/client/login");
   };
 
   if (loading) {
